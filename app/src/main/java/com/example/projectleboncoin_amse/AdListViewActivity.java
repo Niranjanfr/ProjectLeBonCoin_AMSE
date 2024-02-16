2c9d08229ad78384cbacccce18b34a8aa703f2bd
Niranjanfr <niranjan.kulkarni@etu.imt-nord-europe.fr>
2024-02-16 09:10:57 +0100

    ajout fichier de base

---
+++ app/src/main/java/com/example/projectleboncoin_amse/AdListViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.projectleboncoin_amse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdListViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ad_list_view);
+    }
+}
