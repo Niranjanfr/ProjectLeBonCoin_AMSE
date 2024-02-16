8338036cf376f7208b4f380a09406b39b2550929
Niranjanfr <niranjan.kulkarni@etu.imt-nord-europe.fr>
2024-02-16 09:37:57 +0100

    travail sur main activity

--- app/src/main/java/com/example/projectleboncoin_amse/MainActivity.java
+++ app/src/main/java/com/example/projectleboncoin_amse/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.projectleboncoin_amse;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android. widget. EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button button9;
+    Button button10;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button9 = (Button) findViewById(R.id.button9);
+        button10 = (Button) findViewById(R.id.button10);
     }
 }
